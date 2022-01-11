67ea690ba33d1ca979b275a2750895ac249f4c58
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-01-11 12:13:04 +0530

    Add files via upload
    program to  find String  array length in java

---
+++ ArrayString(Size).java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class StringArrays{
+    public static void main(String args[]){
+        String arr1[]={"Know","Program"};
+        System.out.println("Size of array list:"+arr1.length);
+        String arr2[]={"Sunil","Harish","Rpg"};
+        System.out.println("Size of an array list:"+arr2.length);
+        String arr3[]={"C","C++","Java","Python"};
+        System.out.println("Size of an array in list:"+arr3.length);
+    }
+}
