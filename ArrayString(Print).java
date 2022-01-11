148e44cf5c44fb43a014661e11496ad6e88ddebe
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-01-11 12:19:38 +0530

    Add files via upload
    Program to print String in array in java

---
+++ ArrayString(Print).java
@@ -0,0 +1,8 @@
+public class StringArrays{
+    public static void main(String args[]){
+        String lang[]={"Java","Python","C++"};
+        for(int i=0;i<lang.length;i++){
+            System.out.println("Display arrays are :"+lang[i]);
+        }
+    }
+}
