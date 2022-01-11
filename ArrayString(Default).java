a41839239130ef9ec01aade1c603c57335aca32b
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-01-11 12:32:55 +0530

    Add files via upload
    Program to initilization String in array with  default value and initilazation in array in java

---
+++ ArrayString(Default).java
@@ -0,0 +1,14 @@
+public class Stringarrays{
+    public static void main(String args[]){
+        String lang[]=new String[5];
+        System.out.println("Default value in array Element:");
+        for(int i=0;i<lang.length;i++){
+            System.out.println(lang[i]);
+        }
+        String lang1[]={"Java","Programmer","BikeLover"};
+        System.out.println("Initilisation of array :");
+        for(int i=0;i<lang1.length;i++){
+            System.out.println(lang1[i]);
+        }
+    }
+}
